997d574badf70990bb211187fe5395ef72449fbf
Matthew P Kemp <matthew.p.kemp@gmail.com>
2019-11-07 08:30:05 +0200

    Removed unnecessary condition for cacheDir checking

--- app/src/main/java/com/nuclearsunrise/uscishippo/apiutils/APIhandler.kt
+++ app/src/main/java/com/nuclearsunrise/uscishippo/apiutils/APIhandler.kt
@@ -15,31 +15,17 @@ object APIhandler {
     val apiInterface: APIinterface by lazy {
         Log.d("APIhanlder", "Creating Client")
 
-        if (cacheDir != null)
-        {
-            Log.d("APIhandler", "Cache Directory set")
+        Log.d("APIhandler", "Cache Directory set")
 
-            val cacheSize: Long = 5 * 1024 * 1024 // 5MB should be more than enough
-            val cache = Cache(this.cacheDir, cacheSize)
-            val okHttpClient = OkHttpClient.Builder()
-                .cache(cache)
-                .build()
+        val cacheSize: Long = 5 * 1024 * 1024 // 5MB should be more than enough
+        val cache = Cache(this.cacheDir, cacheSize)
+        val okHttpClient = OkHttpClient.Builder()
+            .cache(cache)
+            .build()
 
-            val retrofit = Retrofit.Builder()
-                .baseUrl(STR_BASE_URL)
-                .client(okHttpClient)
-                .addConverterFactory(MoshiConverterFactory.create())
-                .addCallAdapterFactory(CoroutineCallAdapterFactory())
-                .build()
-
-            return@lazy retrofit.create(APIinterface::class.java)
-
-
-        }
-
-        Log.d("APIhandler", "Cache Directory not set")
         val retrofit = Retrofit.Builder()
             .baseUrl(STR_BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(MoshiConverterFactory.create())
             .addCallAdapterFactory(CoroutineCallAdapterFactory())
             .build()
@@ -47,7 +33,7 @@ object APIhandler {
         return@lazy retrofit.create(APIinterface::class.java)
     }
 
-    operator fun invoke(cDir:File): APIhandler {
+    operator fun invoke(cDir: File): APIhandler {
         this.cacheDir = cDir
         return this
     }
